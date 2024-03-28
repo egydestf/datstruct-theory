26eb6a3d13cfa48c71d39ff711f147479bc334a2
egydestf <egydestiar06@gmail.com>
2024-03-28 22:35:35 +0700

    dupchecker

---
+++ Cek Duplikat Array/hashCekDuplikat.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+bool cekDuplikat(int arr[]) {
+    unordered_set<int> seen;
+    int panjangArr = 4;
+    for (int i = 0; i < panjangArr; i++) {
+        if (seen.find(arr[i]) != seen.end()) {
+            return true; // Duplicate found
+        }
+        seen.insert(arr[i]); // Insert the element into the set
+    }
+    return false; // No duplicate found
+}
+
+int main(void) {
+    int arrA[] = {2, 1, 3, 1};
+    cout << cekDuplikat(arrA);
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+<pre>
+Nama    : Egy Destiar Firmandani
+NIM     : 2311110061
+Kelas   : S1SD-04-B
+</pre>
